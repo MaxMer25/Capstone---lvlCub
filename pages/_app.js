4bd7d4c969f95eb309f2e277ac1c91082cdce714
Maximilian Mergenthal <max-mergenthal@gmx.de>
2022-11-29 15:04:12 +0100

    navigationUpdate

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
 import GlobalStyles from "../styles/GlobalStyles";
 import Navigation from "../components/Navigation/Navigation";
-import {useState} from "react";
 
 function MyApp({Component, pageProps}) {
-  const [currentPage, setCurrentPage] = useState("home");
   return (
     <>
       <GlobalStyles />
       <Component {...pageProps} />
-      <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <Navigation />
     </>
   );
 }
