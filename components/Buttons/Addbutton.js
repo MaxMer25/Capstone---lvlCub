fd6c13453e524f0e6fa259e1fe6c7301687a4863
Maximilian Mergenthal <max-mergenthal@gmx.de>
2023-01-05 22:51:24 +0100

    fixed typo

--- components/Buttons/Addbutton.js
+++ components/Buttons/Addbutton.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Link from "next/link";
 
-export default function Addbutton({endpoint}) {
+export default function AddButton({endpoint}) {
   return (
     <Link href={endpoint}>
       <StyledSvg
